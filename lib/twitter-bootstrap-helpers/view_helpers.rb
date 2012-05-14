def8e8db0ab18acf8d7342532825a4c565358b48
Bradly Feeley <bradlyf@gmail.com>
2012-05-14 13:28:26 -0700

    Fixing bug where alert helpers weren't in the correct module.

--- lib/twitter-bootstrap-helpers/view_helpers.rb
+++ lib/twitter-bootstrap-helpers/view_helpers.rb
@@ -3,33 +3,33 @@ module TwitterBootstrapHelpers
     def icon_for(icon_name)
       content_tag :i, '', :class => "icon-#{icon_name}"
     end
-  end
 
-  def alert_message(opts = {}, &block)
-    alert_classes = ["alert"]
-    alert_classes << "alert-#{opts[:type]}" if opts[:type]
-    alert_classes << opts[:class] if opts[:class]
+    def alert_message(opts = {}, &block)
+      alert_classes = ["alert"]
+      alert_classes << "alert-#{opts[:type]}" if opts[:type]
+      alert_classes << opts[:class] if opts[:class]
 
-    contents = capture(&block)
+      contents = capture(&block)
 
-    if opts[:heading]
-      alert_classes << "alert-block"
-      contents = alert_heading_tag + contents
+      if opts[:heading]
+        alert_classes << "alert-block"
+        contents = alert_heading_tag + contents
+      end
+
+      if opts[:show_close_button]
+        contents = alert_close_tag + contents
+      end
+
+      content_tag :div, contents, :class => alert_classes.join(' ')
     end
 
-    if opts[:show_close_button]
-      contents = alert_close_tag + contents
+    def alert_close_tag
+      content_tag(:a, 'x', :class => 'close', :data => {:dismiss => 'alert'})
     end
 
-    content_tag :div, contents, :class => alert_classes.join(' ')
-  end
+    def alert_heading_tag(heading)
+      contents = content_tag(:h4, heading, :class => 'alert-heading')
+    end
 
-  def alert_close_tag
-    content_tag(:a, 'x', :class => 'close', :data => {:dismiss => 'alert'})
   end
-
-  def alert_heading_tag(heading)
-    contents = content_tag(:h4, heading, :class => 'alert-heading')
-  end
-
 end
