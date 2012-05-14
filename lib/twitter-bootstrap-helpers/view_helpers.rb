83b90e3d22154eff62e544de965990eb52419cfa
Bradly Feeley <bradlyf@gmail.com>
2012-05-14 10:24:09 -0700

    Adding helper for displaying alert messages.

--- lib/twitter-bootstrap-helpers/view_helpers.rb
+++ lib/twitter-bootstrap-helpers/view_helpers.rb
@@ -4,4 +4,32 @@ module TwitterBootstrapHelpers
       content_tag :i, '', :class => "icon-#{icon_name}"
     end
   end
+
+  def alert_message(opts = {}, &block)
+    alert_classes = ["alert"]
+    alert_classes << "alert-#{opts[:type]}" if opts[:type]
+    alert_classes << opts[:class] if opts[:class]
+
+    contents = capture(&block)
+
+    if opts[:heading]
+      alert_classes << "alert-block"
+      contents = alert_heading_tag + contents
+    end
+
+    if opts[:show_close_button]
+      contents = alert_close_tag + contents
+    end
+
+    content_tag :div, contents, :class => alert_classes.join(' ')
+  end
+
+  def alert_close_tag
+    content_tag(:a, 'x', :class => 'close', :data => {:dismiss => 'alert'})
+  end
+
+  def alert_heading_tag(heading)
+    contents = content_tag(:h4, heading, :class => 'alert-heading')
+  end
+
 end
