8d0f0ae216d0cd0bba31ef6d09427c92b708f071
Kristian Mandrup <kmandrup@gmail.com>
2012-12-03 16:37:43 +0100

    fix progress bar bugs

--- lib/twitter-bootstrap-helpers/view_helpers/progress.rb
+++ lib/twitter-bootstrap-helpers/view_helpers/progress.rb
@@ -10,34 +10,37 @@ module TwitterBootstrapHelpers
         clazz << " progress-striped" if options[:striped]
         clazz << " active" if options[:active]
 
+        puts "ts_progress: #{clazz} #{options}"
+
         type = options[:type]
         TwitterBoots.validate_type!(type) if type
-        clazz << " progress-#{type}}" if type
+        clazz << " progress-#{type}" if type
 
         content = block_given? ? capture(&block) : options[:txt]
 
         options = options[:html_options] || {}
         options.merge!(class: clazz)
 
+        puts "ts_progress: #{content} #{options}"
+
         content_tag :div, content, options
       end
 
       # <div class="bar" style="width: 60%;"></div>
       def ts_progress_bar width, options = {}, &block
+        type = options.delete(:type)
+        clazz = "bar"
 
-
-        type = options[:type]
-        TwitterBoots.validate_type!(type) if type
-        clazz << " bar-#{type}}" if type
-
-        bar_options = options[:bar_options] || {}
+        if type
+          TwitterBoots.validate_type!(type) 
+          clazz << " bar-#{type}" if type
+        end
 
         unless (0..100).cover? width.to_i
           raise ArgumentError, "Not a valid percentage width: #{width}"
         end
 
-        options = {style: "width: #{width}%"}
-        options.merge!(options[:html_options] || {})
+        options.merge!(style: "width: #{width}%")
         options.merge!(class: clazz)
 
         content = block_given? ? capture(&block) : options[:txt]
